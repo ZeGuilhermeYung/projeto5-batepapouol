9b0ec29fee6b51b8b9ce41266f9f51f7d52fc194
ZeGuilhermeYung <jgryung@gmail.com>
2022-06-22 16:49:56 -0300

    transição entre telas e topo de tela do bate-papo

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let turnAngle = 0;
+let rotateAngle = 0;
 
-setInterval(rotateImg, 170);
+setInterval(rotateImg, 150);
 
 function rotateImg () {
-    turnAngle = turnAngle + 45;
-    document.querySelector(".autentication-screen img:nth-child(4)").setAttribute("style", "transform: rotate(" + turnAngle + "deg)");
-    document.querySelector(".autentication-screen img:nth-child(4)").classList.toggle("rotate");
+    rotateAngle = rotateAngle + 45;
+    if (rotateAngle == 360) {
+        rotateAngle = 0;
+    }
+    document.querySelector(".autentication-screen img:nth-child(4)").setAttribute("style", `transform: rotate(${rotateAngle}deg)`);
+}
+
+function loading () {
+    document.querySelector(".autentication-screen").classList.add("loading");
 }
