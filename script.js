609b433dbfe2e37aef8548288235dfc8cf650bbb
ZeGuilhermeYung <jgryung@gmail.com>
2022-06-23 01:47:51 -0300

    implementando checar nome no Axios

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let rotateAngle = 0;
 
-setInterval(rotateImg, 150);
+function checkNameRequest () {
+    document.querySelector(".autentication-screen").classList.add("loading");
+    setInterval(rotateImg, 150);
+    const userName = {name: document.querySelector(".username-login").value};
+    const request = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", userName);
+    request.then(tratarSucesso);
+    request.catch(tratarError);
+}
 
 function rotateImg () {
     rotateAngle = rotateAngle + 45;
